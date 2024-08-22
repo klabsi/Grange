4fd91f781954c88e651a887cdb5b6e3432e80156
Klaudia <klaud.subs@gmail.com>
2024-08-22 16:33:16 +0200

    Change int type to SERIAL in cowshed db; rewrite all sql commands in upper case

--- infrastructure/home-office/initial_data.sql
+++ infrastructure/home-office/initial_data.sql
@@ -1,75 +1,75 @@
-create table henhouse_report (
-id serial primary key,
-date_of_report date,
-number_of_eggs int,
-number_of_workers int
+CREATE TABLE henhouse_report (
+id SERIAL PRIMARY KEY,
+date_of_report DATE,
+number_of_eggs INT,
+number_of_workers INT
 );
 
-create table cheese_factory_report (
-id serial primary key,
-date_of_report date,
-kg_of_cheese double precision,
-number_of_workers int
+CREATE TABLE cheese_factory_report (
+id SERIAL PRIMARY KEY,
+date_of_report DATE,
+kg_of_cheese DOUBLE PRECISION,
+number_of_workers INT
 );
 
-create table cowshed_report (
-id serial primary key,
-date_of_report date,
-liters_of_milk double precision,
-number_of_workers int
+CREATE TABLE cowshed_report (
+id SERIAL PRIMARY KEY,
+date_of_report DATE,
+liters_of_milk DOUBLE PRECISION,
+number_of_workers INT
 );
 
-create table guardhouse_report (
-id serial primary key,
-date_of_report date,
-number_of_fox_attacks int,
-number_of_workers int
+CREATE TABLE guardhouse_report (
+id SERIAL PRIMARY KEY,
+date_of_report DATE,
+number_of_fox_attacks INT,
+number_of_workers INT
 );
 
-create table weekly_report (
-id serial primary key,
-date_of_report date,
-number_of_eggs_per_person double precision,
-liters_of_milk_per_person double precision,
-number_of_workers_per_fox double precision,
-kg_of_cheese_per_person double precision
+CREATE TABLE weekly_report (
+id SERIAL PRIMARY KEY,
+date_of_report DATE,
+number_of_eggs_per_person DOUBLE PRECISION,
+liters_of_milk_per_person DOUBLE PRECISION,
+number_of_workers_per_fox DOUBLE PRECISION,
+kg_of_cheese_per_person DOUBLE PRECISION
 );
 
-create table henhouse_weekly_report (
-henhouse_report_id int,
-weekly_report_id int,
-primary key (henhouse_report_id, weekly_report_id)
+CREATE TABLE henhouse_weekly_report (
+henhouse_report_id INT,
+weekly_report_id INT,
+PRIMARY KEY (henhouse_report_id, weekly_report_id)
 );
 
-create table cheese_factory_weekly_report (
-cheese_factory_report_id int,
-weekly_report_id int,
-primary key (cheese_factory_report_id, weekly_report_id)
+CREATE TABLE cheese_factory_weekly_report (
+cheese_factory_report_id INT,
+weekly_report_id INT,
+PRIMARY KEY (cheese_factory_report_id, weekly_report_id)
 );
 
-create table cowshed_weekly_report (
-cowshed_report_id int,
-weekly_report_id int,
-primary key (cowshed_report_id, weekly_report_id)
+CREATE TABLE cowshed_weekly_report (
+cowshed_report_id INT,
+weekly_report_id INT,
+PRIMARY KEY (cowshed_report_id, weekly_report_id)
 );
 
-create table guardhouse_weekly_report (
-guardhouse_report_id int,
-weekly_report_id int,
-primary key (guardhouse_report_id, weekly_report_id)
+CREATE TABLE guardhouse_weekly_report (
+guardhouse_report_id INT,
+weekly_report_id INT,
+PRIMARY KEY (guardhouse_report_id, weekly_report_id)
 );
 
-create table monthly_report (
-id serial primary key,
-date_of_report date,
-number_of_eggs_per_person double precision,
-liters_of_milk_per_person double precision,
-number_of_workers_per_fox double precision,
-kg_of_cheese_per_person double precision
+CREATE TABLE monthly_report (
+id SERIAL PRIMARY KEY,
+date_of_report DATE,
+number_of_eggs_per_person DOUBLE PRECISION,
+liters_of_milk_per_person DOUBLE PRECISION,
+number_of_workers_per_fox DOUBLE PRECISION,
+kg_of_cheese_per_person DOUBLE PRECISION
 );
 
-create table weekly_monthly_report (
-weekly_report_id int,
-monthly_report_id int,
-primary key (weekly_report_id, monthly_report_id)
+CREATE TABLE weekly_monthly_report (
+weekly_report_id INT,
+monthly_report_id INT,
+PRIMARY KEY (weekly_report_id, monthly_report_id)
 );
