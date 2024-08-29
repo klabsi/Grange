80a3c8854281f0f5d97a9ab37a1588446b132fe4
Klaudia <klaud.subs@gmail.com>
2024-08-29 11:16:02 +0200

    Change data types to 'var'; extract methods

--- home-office/src/main/java/org/sawaklaudia/ManuallyInsertedData.java
+++ home-office/src/main/java/org/sawaklaudia/ManuallyInsertedData.java
@@ -5,8 +5,6 @@ import org.sawaklaudia.input.CowshedInput;
 import org.sawaklaudia.input.GuardhouseInput;
 import org.sawaklaudia.input.HenhouseInput;
 import org.sawaklaudia.model.*;
-import org.sawaklaudia.output.MonthlyReport;
-import org.sawaklaudia.output.WeeklyReport;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -20,10 +18,10 @@ public class ManuallyInsertedData {
         List<GuardhouseInput> guardhouseInputs = generateGuardhouseData();
         List<HenhouseInput> henhouseInputs = generateHenhouseData();
 
-        CheeseFactoryInputProcessor cheeseFactoryInputProcessor = new CheeseFactoryInputProcessor();
-        CowshedInputProcessor cowshedInputProcessor = new CowshedInputProcessor();
-        HenhouseInputProcessor henhouseInputProcessor = new HenhouseInputProcessor();
-        GuardhouseInputProcessor guardhouseInputProcessor = new GuardhouseInputProcessor();
+        var cheeseFactoryInputProcessor = new CheeseFactoryInputProcessor();
+        var cowshedInputProcessor = new CowshedInputProcessor();
+        var henhouseInputProcessor = new HenhouseInputProcessor();
+        var guardhouseInputProcessor = new GuardhouseInputProcessor();
 
         double kgOfCheesePerMonth = cheeseFactoryInputProcessor.calculateKgOfCheesePerMonth(cheeseFactoryInputs.get(0),
                 cheeseFactoryInputs.get(1), cheeseFactoryInputs.get(2), cheeseFactoryInputs.get(3));
@@ -34,126 +32,80 @@ public class ManuallyInsertedData {
         int numberOfFoxAttacksPerMonth = guardhouseInputProcessor.calculateNumberOfFoxAttacksPerMonth(guardhouseInputs.get(0),
                 guardhouseInputs.get(1), guardhouseInputs.get(2), guardhouseInputs.get(3));
 
-        MonthlyReportFactory monthlyReportFactory = new MonthlyReportFactory();
-        MonthlyReport monthlyReport = monthlyReportFactory.process(numberOfEggsPerMonth, literOfMilkPerMonth, numberOfFoxAttacksPerMonth, kgOfCheesePerMonth);
+        var monthlyReportFactory = new MonthlyReportFactory();
+        var monthlyReport = monthlyReportFactory.process(numberOfEggsPerMonth, literOfMilkPerMonth, numberOfFoxAttacksPerMonth, kgOfCheesePerMonth);
         System.out.println(monthlyReport.toString());
 
         double kgOfCheesePerPerson = cheeseFactoryInputProcessor.processKgOfCheesePerPerson(cheeseFactoryInputs.get(0));
         double literOfMilkPerPerson = cowshedInputProcessor.processLiterOfMilkPerPerson(cowshedInputs.get(0));
         double numberOfEggsPerPerson = henhouseInputProcessor.processNumOfEggsPerPerson(henhouseInputs.get(0));
         double numberOfPersonPerFox = guardhouseInputProcessor.processNumberOfPersonPerFox(guardhouseInputs.get(0));
-        WeeklyReportFactory weeklyReportFactory = new WeeklyReportFactory();
-        WeeklyReport weekOneReport = weeklyReportFactory.process(numberOfEggsPerPerson, literOfMilkPerPerson, numberOfPersonPerFox, kgOfCheesePerPerson);
+        var weeklyReportFactory = new WeeklyReportFactory();
+        var weekOneReport = weeklyReportFactory.process(numberOfEggsPerPerson, literOfMilkPerPerson, numberOfPersonPerFox, kgOfCheesePerPerson);
         System.out.println(weekOneReport.toString());
     }
 
     public static List<CheeseFactoryInput> generateCheeseFactoryData() {
-        CheeseFactoryInput cheeseFactoryInput1 = CheeseFactoryInput.builder()
-                .kgOfCheesePerWeek(12)
-                .numberOfWorkersPerWeek(2)
-                .dateOfReport(LocalDate.of(2024, 6,3))
-                .build();
-
-        CheeseFactoryInput cheeseFactoryInput2 = CheeseFactoryInput.builder()
-                .kgOfCheesePerWeek(11)
-                .numberOfWorkersPerWeek(2)
-                .dateOfReport(LocalDate.of(2024, 6, 10))
-                .build();
-
-        CheeseFactoryInput cheeseFactoryInput3 = CheeseFactoryInput.builder()
-                .kgOfCheesePerWeek(4)
-                .numberOfWorkersPerWeek(2)
-                .dateOfReport(LocalDate.of(2024, 6,17))
-                .build();
-
-        CheeseFactoryInput cheeseFactoryInput4 = CheeseFactoryInput.builder()
-                .kgOfCheesePerWeek(8)
-                .numberOfWorkersPerWeek(2)
-                .dateOfReport(LocalDate.of(2024, 6,24))
-                .build();
-
+        var cheeseFactoryInput1 = createCheeseFactoryInput(12, 7);
+        var cheeseFactoryInput2 = createCheeseFactoryInput(14, 17);
+        var cheeseFactoryInput3 = createCheeseFactoryInput(11, 21);
+        var cheeseFactoryInput4 = createCheeseFactoryInput(10, 28);
         return List.of(cheeseFactoryInput1, cheeseFactoryInput2, cheeseFactoryInput3, cheeseFactoryInput4);
     }
 
+    private static CheeseFactoryInput createCheeseFactoryInput(double kgOfCheesePerWeek, int dayOfMonth) {
+        return CheeseFactoryInput.builder()
+                .kgOfCheesePerWeek(kgOfCheesePerWeek)
+                .numberOfWorkersPerWeek(2)
+                .dateOfReport(LocalDate.of(2024, 6, dayOfMonth))
+                .build();
+    }
+
     public static List<HenhouseInput> generateHenhouseData() {
-        HenhouseInput henhouseInput1 = HenhouseInput.builder()
-                .numberOfEggsPerWeek(28)
-                .numberOfWorkersPerWeek(4)
-                .dateOfReport(LocalDate.of(2024, 6,3))
-                .build();
-
-        HenhouseInput henhouseInput2 = HenhouseInput.builder()
-                .numberOfEggsPerWeek(20)
-                .numberOfWorkersPerWeek(3)
-                .dateOfReport(LocalDate.of(2024, 6,10))
-                .build();
-
-        HenhouseInput henhouseInput3 = HenhouseInput.builder()
-                .numberOfEggsPerWeek(30)
-                .numberOfWorkersPerWeek(4)
-                .dateOfReport(LocalDate.of(2024, 6,17))
-                .build();
-
-        HenhouseInput henhouseInput4 = HenhouseInput.builder()
-                .numberOfEggsPerWeek(22)
-                .numberOfWorkersPerWeek(4)
-                .dateOfReport(LocalDate.of(2024, 6,24))
-                .build();
-
+        var henhouseInput1 = createHenhouseInput(28, 7);
+        var henhouseInput2 = createHenhouseInput(20, 17);
+        var henhouseInput3 = createHenhouseInput(30,  21);
+        var henhouseInput4 = createHenhouseInput(22, 28);
         return List.of(henhouseInput1, henhouseInput2, henhouseInput3, henhouseInput4);
     }
 
+    private static HenhouseInput createHenhouseInput(int numberOfEggsPerWeek, int dayOfMonth) {
+        return HenhouseInput.builder()
+                .numberOfEggsPerWeek(numberOfEggsPerWeek)
+                .numberOfWorkersPerWeek(4)
+                .dateOfReport(LocalDate.of(2024, 6, dayOfMonth))
+                .build();
+    }
+
     public static List<GuardhouseInput> generateGuardhouseData() {
-        GuardhouseInput guardhouseInput1 = GuardhouseInput.builder()
-                .numberOfFoxAttacksPerWeek(0)
-                .numberOfWorkersPerWeek(2)
-                .dateOfReport(LocalDate.of(2024, 6,3))
-                .build();
-
-        GuardhouseInput guardhouseInput2 = GuardhouseInput.builder()
-                .numberOfFoxAttacksPerWeek(0)
-                .numberOfWorkersPerWeek(2)
-                .dateOfReport(LocalDate.of(2024, 6,10))
-                .build();
-
-        GuardhouseInput guardhouseInput3 = GuardhouseInput.builder()
-                .numberOfFoxAttacksPerWeek(1)
-                .numberOfWorkersPerWeek(2)
-                .dateOfReport(LocalDate.of(2024, 6,17))
-                .build();
-
-        GuardhouseInput guardhouseInput4 = GuardhouseInput.builder()
-                .numberOfFoxAttacksPerWeek(1)
-                .numberOfWorkersPerWeek(2)
-                .dateOfReport(LocalDate.of(2024, 6,24))
-                .build();
+        var guardhouseInput1 = createGuardhouseInput(0, 7);
+        var guardhouseInput2 = createGuardhouseInput(0, 14);
+        var guardhouseInput3 = createGuardhouseInput(1, 21);
+        var guardhouseInput4 = createGuardhouseInput(1, 28);
         return List.of(guardhouseInput1, guardhouseInput2, guardhouseInput3, guardhouseInput4);
     }
 
+    private static GuardhouseInput createGuardhouseInput(int numberOfFoxAttacksPerWeek, int dayOfMonth) {
+        return GuardhouseInput.builder()
+                .numberOfFoxAttacksPerWeek(numberOfFoxAttacksPerWeek)
+                .numberOfWorkersPerWeek(2)
+                .dateOfReport(LocalDate.of(2024, 6, dayOfMonth))
+                .build();
+    }
+
     public static List<CowshedInput> generateCowshedData() {
-        CowshedInput cowshedInput1 = CowshedInput.builder()
-                .litersOfMilkPerWeek(52)
-                .numberOfWorkersPerWeek(8)
-                .dateOfReport(LocalDate.of(2024, 6,3))
-                .build();
-
-        CowshedInput cowshedInput2 = CowshedInput.builder()
-                .litersOfMilkPerWeek(58)
-                .numberOfWorkersPerWeek(9)
-                .dateOfReport(LocalDate.of(2024, 6,10))
-                .build();
-
-        CowshedInput cowshedInput3 = CowshedInput.builder()
-                .litersOfMilkPerWeek(50)
-                .numberOfWorkersPerWeek(8)
-                .dateOfReport(LocalDate.of(2024, 6,17))
-                .build();
-
-        CowshedInput cowshedInput4 = CowshedInput.builder()
-                .litersOfMilkPerWeek(59)
-                .numberOfWorkersPerWeek(8)
-                .dateOfReport(LocalDate.of(2024, 6,24))
-                .build();
+        var cowshedInput1 = createCowshedInput(60, 7);
+        var cowshedInput2 = createCowshedInput(58, 14);
+        var cowshedInput3 = createCowshedInput(49.5, 21);
+        var cowshedInput4 = createCowshedInput(55, 28);
         return List.of(cowshedInput1, cowshedInput2, cowshedInput3, cowshedInput4);
+    }
+
+    private static CowshedInput createCowshedInput(double litersOfMilkPerWeek, int dayOfMonth) {
+        return CowshedInput.builder()
+                .litersOfMilkPerWeek(litersOfMilkPerWeek)
+                .numberOfWorkersPerWeek(8)
+                .dateOfReport(LocalDate.of(2024, 6, dayOfMonth))
+                .build();
     }
 }
