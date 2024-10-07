4ffae61847e27d26b1d6b2147a723d092cddd033
Klaudia <klaud.subs@gmail.com>
2024-10-07 17:46:06 +0200

    update method by calculating average from list of 7 inputs; update tests to this method

--- home-office/src/main/java/org/sawaklaudia/model/CowshedInputProcessor.java
+++ home-office/src/main/java/org/sawaklaudia/model/CowshedInputProcessor.java
@@ -2,9 +2,11 @@ package org.sawaklaudia.model;
 
 import org.sawaklaudia.input.CowshedInput;
 
+import java.util.List;
+
 public class CowshedInputProcessor {
 
-    public double calculateLitersOfMilkPerMonth(CowshedInput cowshedInput1, CowshedInput cowshedInput2, CowshedInput cowshedInput3,
+    public double calcTotalLitersOfMilkPerMonth(CowshedInput cowshedInput1, CowshedInput cowshedInput2, CowshedInput cowshedInput3,
                                                 CowshedInput cowshedInput4) {
         if (cowshedInput1 == null || cowshedInput2 == null || cowshedInput3 == null || cowshedInput4 == null) {
             throw new IllegalArgumentException("Cowshed input cannot be null.");
@@ -14,8 +16,15 @@ public class CowshedInputProcessor {
                 + cowshedInput3.getLitersOfMilkPerWeek() + cowshedInput4.getLitersOfMilkPerWeek();
     }
 
-    public double processLiterOfMilkPerPerson(CowshedInput cowshedInput) {
-        if (cowshedInput == null) throw new IllegalArgumentException("Cowshed input cannot be null.");
-        return cowshedInput.getLitersOfMilkPerWeek() / cowshedInput.getNumberOfWorkersPerWeek();
+    public double calcLitersOfMilkPerWorkerPerWeek(List<CowshedInput> cowshedInputsFromAWeek) {
+        if (cowshedInputsFromAWeek == null) throw new IllegalArgumentException("Cowshed inputs list cannot be null.");
+
+        double totalLitersOfMilk = 0;
+        int totalNumberOfWorkers = 0;
+        for (CowshedInput input : cowshedInputsFromAWeek) {
+            totalLitersOfMilk += input.getLitersOfMilkPerWeek();
+            totalNumberOfWorkers += input.getNumberOfWorkersPerWeek();
+        }
+        return totalLitersOfMilk/totalNumberOfWorkers;
     }
 }
