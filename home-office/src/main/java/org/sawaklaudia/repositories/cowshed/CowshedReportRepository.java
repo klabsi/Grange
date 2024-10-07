3e7be873ff103420db89a83a9508cc5975aa9929
Klaudia <klaud.subs@gmail.com>
2024-10-07 17:26:16 +0200

    create a method to get all 7 entries from the last week

--- home-office/src/main/java/org/sawaklaudia/repositories/cowshed/CowshedReportRepository.java
+++ home-office/src/main/java/org/sawaklaudia/repositories/cowshed/CowshedReportRepository.java
@@ -2,8 +2,15 @@ package org.sawaklaudia.repositories.cowshed;
 
 import org.sawaklaudia.domain.cowshed.CowshedReportEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CowshedReportRepository extends JpaRepository<CowshedReportEntity, Long> {
+
+    @Query(nativeQuery = true, value = "SELECT * FROM cowshed_report WHERE date_of_report BETWEEN :startDate AND :endDate")
+    public List<CowshedReportEntity> findAllReportsOfAWeek(@Param("startDate") String startDate, @Param("endDate") String endDate);
 }
