34d4420a17b1d8eaa6486b5f8c8711e912340aa8
Klaudia <klaud.subs@gmail.com>
2024-10-07 17:23:01 +0200

    insert cowshed data from a week into cowshed_report database

--- home-office/src/main/java/org/sawaklaudia/SpringInsertedData.java
+++ home-office/src/main/java/org/sawaklaudia/SpringInsertedData.java
@@ -1,11 +1,16 @@
 package org.sawaklaudia;
 
 import org.sawaklaudia.domain.cheesefactory.CheeseFactoryWeeklyReportEntity;
+import org.sawaklaudia.domain.cowshed.CowshedReportEntity;
+import org.sawaklaudia.model.CowshedInputProcessor;
+import org.sawaklaudia.repositories.cheesefactory.CheeseFactoryReportRepository;
 import org.sawaklaudia.repositories.cheesefactory.CheeseFactoryWeeklyReportRepository;
+import org.sawaklaudia.repositories.cowshed.CowshedReportRepository;
 import org.sawaklaudia.repositories.cowshed.CowshedWeeklyReportRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class SpringInsertedData implements AppLaunchType {
@@ -19,5 +24,28 @@ public class SpringInsertedData implements AppLaunchType {
 
         CowshedWeeklyReportRepository cowshedWeeklyReportRepository = context.getBean(CowshedWeeklyReportRepository.class);
         System.out.println(cowshedWeeklyReportRepository.findAll());
+
+        CheeseFactoryReportRepository cheeseFactoryReportRepository = context.getBean(CheeseFactoryReportRepository.class);
+        System.out.println(cheeseFactoryReportRepository.findAll());
+
+        CowshedReportRepository cowshedReportRepository = context.getBean(CowshedReportRepository.class);
+        cowshedReportRepository.save(buildCowshedData(LocalDate.of(2024, 10, 1), 5, 48));
+        cowshedReportRepository.save(buildCowshedData(LocalDate.of(2024, 10, 2), 4, 52));
+        cowshedReportRepository.save(buildCowshedData(LocalDate.of(2024, 10, 3), 6, 68));
+        cowshedReportRepository.save(buildCowshedData(LocalDate.of(2024, 10, 4), 5, 42));
+        cowshedReportRepository.save(buildCowshedData(LocalDate.of(2024, 10, 5), 5, 51));
+        cowshedReportRepository.save(buildCowshedData(LocalDate.of(2024, 10, 6), 6, 62));
+        cowshedReportRepository.save(buildCowshedData(LocalDate.of(2024, 10, 7), 5, 42));
+
+        CowshedInputProcessor cowshedInputProcessor = new CowshedInputProcessor();
+        //cowshedInputProcessor.calcLitersOfMilkPerWorkerPerWeek();
+    }
+
+    private static CowshedReportEntity buildCowshedData(LocalDate dateOfReport, int numberOfWorkers, double litersOfMilk) {
+        return CowshedReportEntity.builder()
+                .dateOfReport(dateOfReport)
+                .numberOfWorkers(numberOfWorkers)
+                .litersOfMilk(litersOfMilk)
+                .build();
     }
 }
