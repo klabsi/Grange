43d02e3be1a8c6a1b22abef90f364b8ed68463a8
Klaudia <klaud.subs@gmail.com>
2024-10-31 09:37:07 +0100

    Fix a bash script

--- infrastructure/script.sh
+++ infrastructure/script.sh
@@ -6,13 +6,18 @@ docker stop home-office-service
 echo "Usuwanie kontenera home-office-service..."
 docker rm home-office-service
 
+echo "Usuwanie obrazu home-office-service..."
+docker rmi grange-home-office-service
+
 echo "Uruchamianie polecenia clean dla home-office..."
-../gradlew home-office:clean
+cd ..
+./gradlew home-office:clean
 
 echo "Uruchamianie polecenia bootJar dla home-office..."
-../gradlew home-office:bootJar
+./gradlew home-office:bootJar
 
 echo "Uruchamianie docker-compose..."
+cd infrastructure/
 docker-compose up -d
 
 echo "Skrypt zakończony."
