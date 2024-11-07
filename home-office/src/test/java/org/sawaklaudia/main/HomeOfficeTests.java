3409142c4bcdea2b9806510576c3a42b463019d2
Klaudia <klaud.subs@gmail.com>
2024-11-07 11:38:17 +0100

    Test profile

--- home-office/src/test/java/org/sawaklaudia/main/HomeOfficeTests.java
+++ home-office/src/test/java/org/sawaklaudia/main/HomeOfficeTests.java
@@ -2,7 +2,9 @@ package org.sawaklaudia.main;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
+@Profile("test")
 @SpringBootTest
 public class HomeOfficeTests {
 
