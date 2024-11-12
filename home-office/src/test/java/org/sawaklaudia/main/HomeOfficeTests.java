fca21ae8cc977a9bb6d72d6e06bc878968d7129f
Klaudia <klaud.subs@gmail.com>
2024-11-12 19:07:11 +0100

    Add @DataJpaTest annotation to a test

--- home-office/src/test/java/org/sawaklaudia/main/HomeOfficeTests.java
+++ home-office/src/test/java/org/sawaklaudia/main/HomeOfficeTests.java
@@ -1,9 +1,11 @@
 package org.sawaklaudia.main;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 
+@DataJpaTest
 @Profile("test")
 @SpringBootTest
 public class HomeOfficeTests {
