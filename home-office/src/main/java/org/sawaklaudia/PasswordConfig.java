d1a2e428697e40c3b547ad7d844d56d6f7cc0c91
Klaudia <klaud.subs@gmail.com>
2024-12-31 08:54:21 +0100

    Add Jasypt

---
+++ home-office/src/main/java/org/sawaklaudia/PasswordConfig.java
@@ -0,0 +1,17 @@
+package org.sawaklaudia;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+
+    private static final int ENCODER_STRENGTH = 10;
+
+    @Bean
+    public PasswordEncoder createPasswordEncoder() {
+        return new BCryptPasswordEncoder(ENCODER_STRENGTH);
+    }
+}
